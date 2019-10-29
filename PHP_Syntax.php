366434d4ca8af3915d567bccdf0f47499a0f28e0
Tae Lee <taebumlee@gmail.com>
2019-10-29 10:24:55 -0700

    This is first commit for php study

---
+++ PHP_Syntax.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php
+echo "My first PHP script!<br>";
+echo "A PHP file normally contains HTML tags, and some PHP scripting code.<br>";
+ECHO "Hello World!<br>";
+echo "Hello World!<br>";
+EcHo "Hello World!<br>";
+?>
+
+<?php
+echo "This should be new line..."
+?>
+
+</body>
+</html>
